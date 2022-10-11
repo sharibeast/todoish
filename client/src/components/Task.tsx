0b6074f5891b263c48f89c4fc3a4183e5572c4cd
Sharif Ally <50718667+sharibeast@users.noreply.github.com>
2022-10-11 16:59:16 +0300

    fix: dont edit task when completed

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -70,12 +70,16 @@ export default function Task({ todo, checked, id, onChange, onDelete, onUpdate, 
                                         {
                                                   showDelete && (
                                                             <div className='absolute right-1 top-2 '>
-                                                                      <button onClick={showEditingForm}>
-                                                                                <svg width="24" height="24" fill="none" stroke='currentColor' className='text-gray-500' viewBox="0 0 24 24">
-                                                                                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.75 19.25L9 18.25L18.2929 8.95711C18.6834 8.56658 18.6834 7.93342 18.2929 7.54289L16.4571 5.70711C16.0666 5.31658 15.4334 5.31658 15.0429 5.70711L5.75 15L4.75 19.25Z"></path>
-                                                                                          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M19.25 19.25H13.75"></path>
-                                                                                </svg>
-                                                                      </button>
+                                                                      {
+                                                                                !checked && (
+                                                                                          <button onClick={showEditingForm}>
+                                                                                                    <svg width="24" height="24" fill="none" stroke='currentColor' className='text-gray-500' viewBox="0 0 24 24">
+                                                                                                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M4.75 19.25L9 18.25L18.2929 8.95711C18.6834 8.56658 18.6834 7.93342 18.2929 7.54289L16.4571 5.70711C16.0666 5.31658 15.4334 5.31658 15.0429 5.70711L5.75 15L4.75 19.25Z"></path>
+                                                                                                              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M19.25 19.25H13.75"></path>
+                                                                                                    </svg>
+                                                                                          </button>
+                                                                                )
+                                                                      }
                                                                       <button onMouseOver={show} className='ml-auto z-10' onClick={onDelete}>
                                                                                 <svg xmlns="http://www.w3.org/2000/svg"
                                                                                           width="24" height="24"
